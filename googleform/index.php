7b74c66038e2cef44b6a5ccbf55b3818282616b8
Benn King <bennkingy@gmail.com>
2018-12-16 17:00:03 +0000

    Forgot to update email :P

--- googleform/index.php
+++ googleform/index.php
@@ -1,37 +1,37 @@
 <?php
-if(isset($_POST['submit'])):
-    if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])):
-		//your site secret key
+if (isset($_POST['submit'])):
+    if (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])):
+        //your site secret key
         $secret = '6LcvJ4IUAAAAACaSLBe_Sskxc8XbeOYlPGyOYJjN';
-		//get verify response data
+        //get verify response data
         $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
         $responseData = json_decode($verifyResponse);
-		
-		$name = !empty($_POST['name'])?$_POST['name']:'';
-		$email = !empty($_POST['email'])?$_POST['email']:'';
-		$message = !empty($_POST['message'])?$_POST['message']:'';
-        if($responseData->success):
-			//contact form submission code
-			$to = 'codexworld@gmail.com';
-			$subject = 'New contact form have been submitted';
-			$htmlContent = "
+        
+        $name = !empty($_POST['name'])?$_POST['name']:'';
+        $email = !empty($_POST['email'])?$_POST['email']:'';
+        $message = !empty($_POST['message'])?$_POST['message']:'';
+        if ($responseData->success):
+            //contact form submission code
+            $to = 'bennkingy@gmail.com';
+            $subject = 'New contact form have been submitted';
+            $htmlContent = "
 				<h1>Contact request details</h1>
 				<p><b>Name: </b>".$name."</p>
 				<p><b>Email: </b>".$email."</p>
 				<p><b>Message: </b>".$message."</p>
 			";
-			// Always set content-type when sending HTML email
-			$headers = "MIME-Version: 1.0" . "\r\n";
-			$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-			// More headers
-			$headers .= 'From:'.$name.' <'.$email.'>' . "\r\n";
-			//send email
-			@mail($to,$subject,$htmlContent,$headers);
-			
+            // Always set content-type when sending HTML email
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            // More headers
+            $headers .= 'From:'.$name.' <'.$email.'>' . "\r\n";
+            //send email
+            @mail($to, $subject, $htmlContent, $headers);
+            
             $succMsg = 'Your contact request have submitted successfully.';
-			$name = '';
-			$email = '';
-			$message = '';
+            $name = '';
+            $email = '';
+            $message = '';
         else:
             $errMsg = 'Robot verification failed, please try again.';
         endif;
@@ -41,9 +41,9 @@ if(isset($_POST['submit'])):
 else:
     $errMsg = '';
     $succMsg = '';
-	$name = '';
-	$email = '';
-	$message = '';
+    $name = '';
+    $email = '';
+    $message = '';
 endif;
 ?>
 <html>
@@ -56,8 +56,8 @@ endif;
     <div class="registration">
 		<h2>Contact Form</h2>
 		<div class="avtar"><img src="images/color.jpg" /></div>
-        <?php if(!empty($errMsg)): ?><div class="errMsg"><?php echo $errMsg; ?></div><?php endif; ?>
-        <?php if(!empty($succMsg)): ?><div class="succMsg"><?php echo $succMsg; ?></div><?php endif; ?>
+        <?php if (!empty($errMsg)): ?><div class="errMsg"><?php echo $errMsg; ?></div><?php endif; ?>
+        <?php if (!empty($succMsg)): ?><div class="succMsg"><?php echo $succMsg; ?></div><?php endif; ?>
 		<div class="form-info">
 			<form action="" method="POST">
 				<input type="text" class="text" value="<?php echo !empty($name)?$name:''; ?>" placeholder="Your full name" name="name" >
